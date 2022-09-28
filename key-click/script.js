d27f5f5e52cab18fb010414d0607605afa3f1fa1
d hendrix <104184081+dHendrixSeven@users.noreply.github.com>
2022-09-28 15:18:07 -0500

    nothing

--- key-click/script.js
+++ key-click/script.js
@@ -54,3 +54,11 @@ setInterval (function() {
     document.title = score + " keyPoints";
 }, 1000);
 
+const menu_toggle = document.querySelector('.menu-toggle');
+const sidebar = document.querySelector('.sidebar');
+
+menu_toggle.addEventListener('click', () => {
+    menu_toggle.classList.toggle('is-active');
+    sidebar.classList.toggle('is-active');
+});
+
